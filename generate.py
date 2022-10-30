ff389e608bd2f387bbca0ce96871a5ebec4c5990
Enric Tobella <etobella@creublanca.es>
2022-10-30 19:22:46 +0100

    Team management

--- generate.py
+++ generate.py
@@ -2,6 +2,9 @@ import yaml
 import github3
 import sys
 from github3.exceptions import NotFoundError
+import logging
+
+_logger = logging.getLogger(__name__)
 
 org = sys.argv[1]
 token = sys.argv[2]
@@ -11,13 +14,21 @@ with open("generate.yml", "r") as f:
 
 gh = github3.login(token=token)
 gh_org = gh.organization(org)
+print(data)
 for team in data:
+    _logger.info("Generating team %s" % team)
     try:
         gh_team = gh_org.team_by_name(team)
     except NotFoundError:
         gh_team = gh_org.create_team(team, privacy="closed")
+    members = []
     for member in gh_team.members():
         if member.login not in data[team].users:
+            _logger.info("Revoking membership for %s" % member.login)
             gh_team.revoke_membership(member.login)
+        else:
+            members.append(member.login)
     for user in data[team].users:
-        gh_team.add_or_update_membership(user)
+        if user not in members:
+            _logger.info("Adding membership to %s" % member.login)
+            gh_team.add_or_update_membership(user)
